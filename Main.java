e15d47fb96428c846aa32d4e1d2db25571df7e8f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-11-04 21:06:15 +0530

    All input method

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
+import java.util.*;
+
 class Main{
     public static void main(String[] args) {
-        System.out.println("Simple hellow world ");
+       Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        char b= sc.next().charAt(0);
+        String c = sc.next();
+        double d = sc.nextDouble();
+        System.out.println(a+""+""+b+""+c+""+d);
     }
 }
