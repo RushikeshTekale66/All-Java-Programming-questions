6045646bae39f56b68778ddb6ca91b9bd22d6057
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-11-05 20:36:07 +0530

    Area of Circle

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
-import java.util.*;
+import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        char b= sc.next().charAt(0);
-        String c = sc.next();
-        double d = sc.nextDouble();
-        System.out.println(a+""+""+b+""+c+""+d);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter radius");
+        int r = sc.nextInt();
+        System.out.println("Area of circle is : " + Math.PI*r*r);
     }
 }
