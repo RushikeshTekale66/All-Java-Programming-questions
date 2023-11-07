7c04a806a57480f76c552537efd4d0e988637ab1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-11-07 19:26:50 +0530

    Double the number without using the given integer

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Height");
-        int h = sc.nextInt();
-        System.out.println("Enter Base");
-        int b = sc.nextInt();
-        System.out.println("Area of Triangle is : " + h*b);
+        System.out.println("Enter the number");
+        int a = sc.nextInt();
+        int Doublenumber = a<<1;
+        System.out.println("Double of number is: " + Doublenumber
+        );
     }
 }
