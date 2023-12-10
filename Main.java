37460eb277020b83a774a69e38cc11b0c85e81b2
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-12-10 20:16:54 +0530

    Addition of two number

--- Main.java
+++ Main.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
+        System.out.println("Enter the first number");
         int a = sc.nextInt();
-        int Doublenumber = a<<1;
-        System.out.println("Double of number is: " + Doublenumber);
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+        System.out.print("Addition of two number is: "+ (a+b));
+
     }
 }
