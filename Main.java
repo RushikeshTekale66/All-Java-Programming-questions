826ffebe4d6f82015fb6b1c756d683d4992b024e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-12-13 22:34:10 +0530

    Get input and reverse the string

--- Main.java
+++ Main.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the first number");
-        int a = sc.nextInt();
-        System.out.println("Enter the second number");
-        int b = sc.nextInt();
-        System.out.print("Addition of two number is: "+ (a+b));
-
+        System.out.println("Enter the string");
+        String str = sc.nextLine();
+        String str2 ="";
+        for(int i=str.length()-1; i>=0; i--){
+                str2=str2 + str.charAt(i);
+        }
+        System.out.println(str2);
     }
 }
