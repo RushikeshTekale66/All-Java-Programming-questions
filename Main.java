18d211834628b688fa0665f9a817c74f43a409a0
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-12-21 07:29:52 +0530

    Right angle star pattern

--- Main.java
+++ Main.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
-import java.util.Stack;
 
 class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Stack st = new Stack();
-        st.push(10);
-        st.push(20);
-        st.push(30);
-        st.push(40);
-        System.out.println(st);
+        System.out.println("Enter how many number of rows in pattern you want");
+        int n = sc.nextInt();
+        for(int i=0; i<n; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
