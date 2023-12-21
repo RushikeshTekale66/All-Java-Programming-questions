daec6c643bc0601f17dec98000c6b640c3c6984b
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-12-21 07:23:44 +0530

    Add item in stack

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the string");
-        String str = sc.nextLine();
-        String str2 ="";
-        for(int i=str.length()-1; i>=0; i--){
-                str2=str2 + str.charAt(i);
-        }
-        System.out.println(str2);
+        Stack st = new Stack();
+        st.push(10);
+        st.push(20);
+        st.push(30);
+        st.push(40);
+        System.out.println(st);
     }
 }
