7ed754fa5609bef8914078ee4ee5e6fa40391159
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-12-25 19:42:23 +0530

    Factorial of a given Number

--- Main.java
+++ Main.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
+        System.out.println("Enter the number ");
         int n = sc.nextInt();
-        if (n % 2 == 0) {
-            System.out.print("The number is even");
-        } else {
-            System.out.println("The number is odd");
+        int factorial=1;
+        for(int i=1; i<=n; i++){
+            factorial = factorial*i;
         }
+        System.out.println("Factorial of Number is: "+ factorial);
     }
 }
