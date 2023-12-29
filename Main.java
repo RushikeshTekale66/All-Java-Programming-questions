716a5d1f56aa9f27e39a97ed5b54ef13e5b4b891
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2023-12-29 22:14:44 +0530

    Swaping of two number without using third number

--- Main.java
+++ Main.java
@@ -1,24 +1,19 @@
-class Test{
-    public void add(int a , int b){
-        System.out.println("Test 1 " + (a+b));
-    }
-}
-
-class Test2 extends Test{
-    public void add(int a , int b){
-        System.out.println("Test 2 " + (a+b));
-    }
-}
-
-//Method overriding / run time polymorphism
-//same method with same paramether with different class
+import java.util.Scanner;
 
 class Main{
     public static void main(String [] args){
-        Test T = new Test();
-        T.add(3,4);
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter a : ");
+        int a = sc.nextInt();
+        System.out.println("Enter b : ");
+        int b = sc.nextInt();
+        
+        System.out.println("Values of A & B before swaping is : a="+ a + ", b="+b);
 
-        Test2 T2 = new Test2();
-        T2.add(10, 20);
+        a = a+b;
+        b=a-b;
+        a = a-b;
+        System.out.println("Values of A & B after swaping is : a="+ a + ", b="+ b);
+
     }
 }
