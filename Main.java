959b5c6c7109f467e7e8d79b817a9a822617618f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-02-23 21:20:24 +0530

    Find the array type wether it is even || odd || mix type

--- Main.java
+++ Main.java
@@ -1,24 +1,44 @@
 import java.util.Scanner;
 
-// find Sum of numbers in a given range
+// find the array type wether it is even || odd || mix type
 
 class Main {
-    public static void main(String args []) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the starting number");
-        int a = sc.nextInt();
-        
-        System.out.println("Enter ending number");
-        int b = sc.nextInt();
+        System.out.println("Enter the array length");
+        int n = sc.nextInt();
 
-        int sum =0;
-        for (int i=a; i<=b; i++){
-            sum = sum + i;
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter the next integer");
+            arr[i] = sc.nextInt();
         }
-        
-        System.out.println(sum);
-        
+
+        int oddcount = 0;
+        int evencount = 0;
+        int mixcount = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                evencount++;
+            } else if (arr[i] % 2 != 0) {
+                oddcount++;
+            } else {
+                mixcount++;
+            }
+        }
+
+        System.out.println("Evencount "+evencount + "\nOddcount " + oddcount + "\nMixcount " + mixcount);
+
+        if (evencount == n)
+            System.out.println("Array is of even type");
+        else if (oddcount == n)
+            System.out.println("Array is of odd type");
+        else
+            System.out.println("Array is of mix type");
+
     }
 
 }
