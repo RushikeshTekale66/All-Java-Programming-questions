e6deec2f808ce9ac395e697ff8be8b0c2e9ec98c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-13 21:18:35 +0530

    Sum of array element

--- Main.java
+++ Main.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
+// Sum of array element
 public class Main
 {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Enter the String");
-		String str = sc.nextLine();
-		String str2 = "";
-		for(int i=0; i< str.length()/2; i++){
-			str2 = str2 + str.charAt(i);
+		int [] arr = {1,2,3,4,5,6,7,8,9};
+		int sum =0;
+		for(int i=0; i<arr.length; i++){
+			sum+=arr[i];
 		}
-		System.err.println(str2);
-		
+		System.out.println(sum);	
 		
 	}
 }
