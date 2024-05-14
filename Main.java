9b62af077e9db2ac2992a62b2bc8a05e6eda518d
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-14 18:38:56 +0530

    Max occuring number in array & its count

--- Main.java
+++ Main.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
-// Sum of array element
-public class Main
-{
+
+// Max occuring number in array & its count
+public class Main {
 	public static void main(String[] args) {
-		int [] arr = {1,2,3,4,5,6,7,8,9};
-		int sum =0;
-		for(int i=0; i<arr.length; i++){
-			sum+=arr[i];
+		int[] arr = { 1, 2, 1, 1, 3, 4, 5, 6, 7, 8, 9 };
+		int maxele = 0;
+		int number = 0;
+		for (int i = 0; i < arr.length; i++) {
+			int count = 0;
+			for (int j = i; j < arr.length; j++) {
+				if (arr[i] == arr[j]) {
+					count++;
+				}
+			}
+			if (count > maxele) {
+				maxele = count;
+				number = arr[i];
+			}
 		}
-		System.out.println(sum);	
-		
+		System.out.println("Max occuring number is " + number + " & its count is " + maxele);
+
 	}
 }
