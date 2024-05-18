4784587e9512c461f79ded1ebd7fe79444a8ac71
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-05-18 23:11:20 +0530

    Occurance number in array & its count

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// Max occuring number in array & its count
+// Occurance number in array & its count
 public class Main {
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 1, 1, 3, 4, 5, 6, 7, 8, 9 };
