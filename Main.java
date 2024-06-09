3d216da54e9f429430a6a77a097dd8400e27b669
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-09 23:58:41 +0530

    Print array

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// Print array element in reverse order
+// Print array element 
 public class Main {
 	public static void main(String[] args) {
 		int[] arr = { 1, 2, 1, 1, 3, 4, 5, 6, 7, 8, 9 };
 		for(int i=0; i<arr.length; i++){
-			System.out.print(arr[i] + " ");
+			System.out.print(arr[i] + " "); 
 		}	
 	}
-}
+} 
