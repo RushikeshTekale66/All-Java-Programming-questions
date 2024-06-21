7c3819d84e693c841cbd60b212cf24341916cc45
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-21 09:03:58 +0530

    Reverse string

--- Main.java
+++ Main.java
@@ -1,16 +1,13 @@
-import java.util.ArrayList;
 import java.util.Scanner;
  
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter the Number ");
-		int n = sc.nextInt();
-		
-		int factorial = 1;
-		for(int i=1; i<=n; i++){
-			factorial = factorial*i;
+		String str = "Rushikesh";
+		String str2 ="";
+		for(int i=str.length()-1; i>=0; i--){
+			str2 = str2+str.charAt(i);
 		}
-		System.out.println(factorial);
+		System.out.println(str2);
 	}
 } 
