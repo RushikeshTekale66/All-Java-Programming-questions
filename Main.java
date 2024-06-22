03adfc15f77a1391e33754b97a0014acea34eee8
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-22 08:49:28 +0530

    Reverse half of the string reverse

--- Main.java
+++ Main.java
@@ -4,8 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String str = "Rushikesh";
+		int length = str.length();
 		String str2 ="";
-		for(int i=str.length()-1; i>=0; i--){
+		for(int i=length-1; i>=length/2; i--){
 			str2 = str2+str.charAt(i);
 		}
 		System.out.println(str2);
