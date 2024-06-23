14f38f5f3cfd76816ff139940f974608717f1a7f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-23 08:33:07 +0530

    Print array in reverse manner

--- Main.java
+++ Main.java
@@ -2,13 +2,10 @@ import java.util.Scanner;
  
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String str = "Rushikesh";
-		int length = str.length();
-		String str2 ="";
-		for(int i=length-1; i>=length/2; i--){
-			str2 = str2+str.charAt(i);
+		int[] arr = {1,2,4,5,6,7};
+
+		for(int i=arr.length-1; i>=0; i--){
+			System.out.println(arr[i]);
 		}
-		System.out.println(str2);
 	}
 } 
