acc50a4a0d60183bceee57e5c69cd953ac4515e1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-24 07:34:28 +0530

    print array

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		int[] arr = {1,2,4,5,6,7};
 
-		for(int i=arr.length-1; i>=0; i--){
+		for(int i=0; i<arr.length; i++){
 			System.out.println(arr[i]);
 		}
 	}
