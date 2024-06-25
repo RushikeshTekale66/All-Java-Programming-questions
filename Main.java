2378074cf7f0f174a9182a4108b45c703b922000
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-25 18:22:54 +0530

    Add element in the array element

--- Main.java
+++ Main.java
@@ -4,8 +4,19 @@ public class Main {
 	public static void main(String[] args) {
 		int[] arr = {1,2,4,5,6,7};
 
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter the number to add in the array element : ");
+		int number = sc.nextInt();
+
+		System.out.println("Array before adding element : ");
 		for(int i=0; i<arr.length; i++){
-			System.out.println(arr[i]);
+			System.out.print(arr[i]+", ");
+		}
+
+		System.out.println("\nArray after adding element : ");
+		for(int i=0; i<arr.length; i++){
+			System.out.print(arr[i]+number +", ");
 		}
 	}
 } 
