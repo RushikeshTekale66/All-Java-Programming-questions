8ec0f98b9bfc237c7d1350aa1391a0a2ed73e94a
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-26 23:15:50 +0530

    Some pattern problem

--- Main.java
+++ Main.java
@@ -1,22 +1,38 @@
-import java.util.Scanner;
  
 public class Main {
 	public static void main(String[] args) {
-		int[] arr = {1,2,4,5,6,7};
-
-		Scanner sc = new Scanner(System.in);
-
-		System.out.print("Enter the number to add in the array element : ");
-		int number = sc.nextInt();
-
-		System.out.println("Array before adding element : ");
-		for(int i=0; i<arr.length; i++){
-			System.out.print(arr[i]+", ");
+		int n = 5;
+		
+		System.out.println("First : =>");
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=i; j++){
+				System.out.print("\t" + i);
+			}
+			System.out.println();
 		}
 
-		System.out.println("\nArray after adding element : ");
-		for(int i=0; i<arr.length; i++){
-			System.out.print(arr[i]+number +", ");
+		System.out.println("Second : =>");
+		for(int i=n; i>=1; i--){
+			for(int j=1; j<=i; j++){
+				System.out.print("\t" + i);
+			}
+			System.out.println();
+		}
+
+		System.out.println("Third : =>");
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=i; j++){
+				System.out.print("\t"+j);
+			}
+			System.out.println();
+		}
+
+		System.out.println("Fourth : =>");
+		for(int i=n; i>=1; i--){
+			for(int j=n; j>=i; j--){
+				System.out.print("\t"+j);
+			}
+			System.out.println();
 		}
 	}
 } 
