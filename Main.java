18189644078fae0ba5d182f964b348482c65b1d5
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-27 14:36:03 +0530

    Increasing trangle ABCD

--- Main.java
+++ Main.java
@@ -4,55 +4,52 @@
 public class Main {
 
 	public static void main(String[] args) {
-		int n = 5;
-		System.out.println("1 ****************");
-		for (int i = 1; i <= n; i++) {
-			for (int j = 1; j <= i; j++) {
-				System.out.print(i);
-			}
-			System.out.println();
-		}
+		int n=5; 
 
-		System.out.println("2 ****************");
-		int count = n;
+		System.out.println("1 *********************");
 		for(int i=1; i<=n; i++){
 			for(int j=1; j<=i; j++){
-				System.out.print(count);
-			}
-			count--;
-			System.out.println();
-		}
-
-		System.out.println("3 ****************");
-		for(int i=1; i<=n; i++){
-			for(int j=1; j<=i; j++){
-				System.out.print(j);
+				System.out.print((char)(i+64));
 			}
 			System.out.println();
 		}
 
-		System.out.println("4 ****************");
-		for(int i=1; i<=n; i++){
-			int count2 = i;
-			for(int j=1; j<=i; j++){
-				System.out.print(count2);
-				count2--;
-			}
-			System.out.println();
-		}
-
-		System.out.println("5 ****************");
-		for(int i=1; i<=n; i++){
-			for(int j=1; j<=i; j++){
-				System.out.print(n-j+1);
-			}
-			System.out.println();
-		}
-
-		System.out.println("6 ****************");
+		System.out.println("2 *********************");
 		for(int i=n; i>=1; i--){
 			for(int j=i; j<=n; j++){
-				System.out.print(j);
+				System.out.print((char)(i+96));
+			}
+			System.out.println();
+		}
+
+		System.out.println("3 *********************");
+		for(int i=1; i<=n; i++){
+			for(int j=1; j<=i; j++){
+				System.out.print((char)(j+64));
+			}
+			System.out.println();
+		}
+		
+		System.out.println("4 *********************");
+		for(int i=1; i<=n; i++){
+			for(int j=i; j>=1; j--){
+				System.out.print((char)(j+96));
+			}
+			System.out.println();
+		}
+
+		System.out.println("5 *********************");
+		for(int i=n; i>=1; i--){
+			for(int j=n; j>=i; j--){
+				System.out.print((char)(j+64));
+			}
+			System.out.println();
+		}
+
+		System.out.println("6 *********************");
+		for(int i=n; i>=1; i--){
+			for(int j=i; j<=n; j++){
+				System.out.print((char)(96+j));
 			}
 			System.out.println();
 		}
@@ -60,40 +57,40 @@ public class Main {
 }
 
 /*
-1 ****************
-1
-22
-333
-4444
-55555
-2 ****************
-5
-44
-333
-2222
-11111
-3 ****************
-1
-12
-123
-1234
-12345
-4 ****************
-1
-21
-321
-4321
-54321
-5 ****************
-5
-54
-543
-5432
-54321
-6 ****************
-5
-45
-345
-2345
-12345
+ 1 *********************
+A
+BB
+CCC
+DDDD
+EEEEE
+2 *********************
+e
+dd
+ccc
+bbbb
+aaaaa
+3 *********************
+A
+AB
+ABC
+ABCD
+ABCDE
+4 *********************
+a
+ba
+cba
+dcba
+edcba
+5 *********************
+E
+ED
+EDC
+EDCB
+EDCBA
+6 *********************
+e
+de
+cde
+bcde
+abcde
  */
