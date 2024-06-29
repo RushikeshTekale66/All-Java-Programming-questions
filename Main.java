64ea687155d7c66a044e2a46df2bf0480ad87890
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-06-29 19:13:46 +0530

    check the character is present in string or not

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
+
 class Main{
     public static void main(String [] args){
         String str = "Rushikesh ";
+        Scanner sc = new Scanner (System.in);
+
+        char ch = 'R';
         for(int i=0; i<str.length(); i++){
-            System.out.print(str.charAt(i)+",");
+            if(ch==str.charAt(i)){
+                System.out.println("Matched");
+            }
         }
     }
 }
