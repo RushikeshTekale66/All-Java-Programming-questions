d5a00bf16575a4e34d57e80db53f89c4815fef69
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-01 23:53:51 +0530

    Maximum occuring character

--- Main.java
+++ Main.java
@@ -5,13 +5,21 @@ class Main{
         String str = "Rushikesh ";
         Scanner sc = new Scanner (System.in);
 
-        char ch = 's';
-        int count = 0;
+        String ch = "";
+        int maxcount =0;
+        int maxIndex =0;
         for(int i=0; i<str.length(); i++){
-            if(ch==str.charAt(i)){
-                count ++;
+             int count = 0;
+            for(int j=0; j<str.length(); j++){
+                if(str.charAt(i)==str.charAt(j)){
+                    count++;
+                }
+            }
+            if (count>maxcount){
+                maxcount+=count;
+                maxIndex = i;
             }
         }
-        System.out.println("count of " + ch + " is "+ count);
+        System.out.println("maximum occuring character is : " +  str.charAt(maxIndex+1));
     }
 }
