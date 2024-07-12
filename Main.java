121d34357814d2c1548f35b124c7b078405508a0
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-12 07:57:43 +0530

    Multiplication of two number using method

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main
 {
-    public int sub(int a, int b){
-        return a-b;
+    public int mul(int a, int b){
+        return a*b;
     }   
 	public static void main(String[] args) {
         Main a = new Main();
-        System.out.println("Additino is : " + a.sub(30, 20));
+        System.out.println("Multiplication is : " + a.mul(30, 20));
 	}
 }
