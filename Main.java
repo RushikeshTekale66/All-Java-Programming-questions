2553451af1614861ba779c34bc6ffb20d64765ea
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-12 07:56:03 +0530

    Substraction of two number using method

--- Main.java
+++ Main.java
@@ -1,10 +1,10 @@
 public class Main
 {
-    public int add(int a, int b){
-        return a+b;
+    public int sub(int a, int b){
+        return a-b;
     }   
 	public static void main(String[] args) {
         Main a = new Main();
-        System.out.println("Additino is : " + a.add(10, 20));
+        System.out.println("Additino is : " + a.sub(30, 20));
 	}
 }
