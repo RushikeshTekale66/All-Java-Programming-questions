ef890d7b733d41f021f29972d62294bd4b7ff81e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-12 07:53:50 +0530

    Addition of two number using method

--- Main.java
+++ Main.java
@@ -1,31 +1,10 @@
 public class Main
 {
-    public int square (int s){
-        int area = s*s;
-        return area;
-    }
-    public int rectangle(int l, int w){
-        int area = l*w;
-        return area;
-    }
-    public double triangle(int b, int h){
-        double area = 3.14*(b*h);
-        return area;
-    }
-    public double circle(int r){
-        double area = 3.14*(r*r);
-        return area;
-    }
-    
+    public int add(int a, int b){
+        return a+b;
+    }   
 	public static void main(String[] args) {
-		Main sh = new Main();
-
-		System.out.println("Square area : " + sh.square(10));
-
-		System.out.println("Rectangle area : " + sh.rectangle(10, 10));
-        
-		System.out.println("Triangle area : " + sh.triangle(10, 10));
-
-		System.out.println("Circle area : " + sh.circle(10));
+        Main a = new Main();
+        System.out.println("Additino is : " + a.add(10, 20));
 	}
 }
