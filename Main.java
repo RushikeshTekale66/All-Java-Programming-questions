69daadb529b8993e655cdcd45e0e991f66693972
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-28 20:12:18 +0530

    Multiple catch block for the exception handling

--- Main.java
+++ Main.java
@@ -1,6 +1,21 @@
 class Main{
 	public static void main(String [] args){
+			try {
+				int n=100, x=1;
+				int [] arr = new int [n];
+				for(int i=0; i<=arr.length; i++){
+					arr[i] = i/x;
 
-		System.out.println("Hello world");
+				}
+			}
+			catch (ArrayIndexOutOfBoundsException e) {
+				System.out.println("Array index of bound "+ e);
+			}
+			catch(ArithmeticException e){
+				System.out.println("Arithmetic " + e);
+			}
+			catch(Exception e){
+				System.out.println("Exception " + e);
+			}
 	}
 }
