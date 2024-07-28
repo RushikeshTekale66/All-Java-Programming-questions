128a55d7550cd8cd61b40c0c67b6f294ca39fc0a
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-28 20:24:00 +0530

    final kayword

--- Main.java
+++ Main.java
@@ -1,21 +1,7 @@
 class Main{
 	public static void main(String [] args){
-			try {
-				int n=100, x=1;
-				int [] arr = new int [n];
-				for(int i=0; i<=arr.length; i++){
-					arr[i] = i/x;
-
-				}
-			}
-			catch (ArrayIndexOutOfBoundsException e) {
-				System.out.println("Array index of bound "+ e);
-			}
-			catch(ArithmeticException e){
-				System.out.println("Arithmetic " + e);
-			}
-			catch(Exception e){
-				System.out.println("Exception " + e);
-			}
+		final String str = "Rushikesh";
+		// str = "Tekale"; //Can't reassign
+		System.out.println(str); 
 	}
 }
