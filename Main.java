f86e23bcc52326a0514f5ca47ea6d5d2bbc6d8f1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-30 12:11:55 +0530

    Addition of two array 1-d & 2-d

--- Main.java
+++ Main.java
@@ -1,23 +1,35 @@
 class Main {
 	public static void main(String[] args) {
 		// 1-d array
-		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-		int add1 = 1;
+		int[] arr1 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int[] arr11 = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-		for (int i = 0; i < arr.length; i++) {
-			add1 *= arr[i];
+		int[] result1 = new int[arr1.length];
+
+		for (int i = 0; i < arr1.length; i++) {
+			result1[i] = arr1[i] * arr11[i];
 		}
-		System.out.println("Multiplication of all 1-d array element " + add1);
+		for (int i = 0; i < result1.length; i++) {
+			System.out.print(" " + result1[i]);
+		}
+		System.out.println();
 
 		// 2-d array
 		int[][] arr2 = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
-		int add2 = 1;
+		int[][] arr22 = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
+		
+		int [][] result2 = new int[arr2.length][arr2.length];
 
 		for (int i = 0; i < arr2.length; i++) {
 			for (int j = 0; j < arr2.length; j++) {
-				add2 *= arr2[i][j];
+				result2[i][j] = arr2[i][j] + arr22[i][j];
 			}
 		}
-		System.out.println("Multiplication of all 2 d element " + add2);
+		for(int i=0; i<result2.length; i++){
+			for(int j=0; j<result2.length; j++){
+				System.out.print(" " + result2[i][j]);
+			}
+			System.out.println();
+		}
 	}
 }
