0800bfdd09f7a5f2f21456c64588a99efa490d2b
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-30 11:24:57 +0530

    Exception handling

--- Main.java
+++ Main.java
@@ -1,29 +1,15 @@
 class Main{
 	public static void main(String [] args){
-		int n=100;
-		
-		int [] arr = new int[n];
-		
-		int x=1;
+		int a=10;
+		int b=0;
+		int c=0;
 
 		System.out.println("Start");
-
 		try{
-			for(int i=0; i<=arr.length; i++){
-				arr[i] = i/x;
-			}
+			c=a/b;
 		}
 		catch(ArithmeticException e){
-			//if x=0
 			System.out.println("Got error" + e);
-		}
-		catch(ArrayIndexOutOfBoundsException e){
-			//if i<=arr.length
-			System.out.println("Got error " + e);
-		}
-		catch(Exception e ){
-			//if got another error
-			System.out.println("Got Error " + e);
 		}
 		System.out.println("End");
 	}
