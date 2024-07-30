8cac8a4f87de9c06ef7882dd6bf6f4d3933952ff
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-07-30 12:01:35 +0530

    Addition of all elements of 1-d & 2-d elements

--- Main.java
+++ Main.java
@@ -1,21 +1,23 @@
-class Main{
-    public static void main(String [] args){
-		//1-d array
-        int [] arr= {1,2,3,4,5,6,7,8,9,10};
-        
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]);
-        }
-		System.out.println();
-       
-		//2-d array
-        int[][] arr2 = {{1,2,3},{1,2,3},{1,2,3}};
-        
-        for(int i=0; i<arr2.length; i++){
-            for(int j=0; j<arr2.length; j++){
-                System.out.print(arr2[i][j]);
-            }
-            System.out.println();
-        }
-    }
+class Main {
+	public static void main(String[] args) {
+		// 1-d array
+		int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		int add1 = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			add1 += arr[i];
+		}
+		System.out.println("Addition of all 1-d array element " + add1);
+
+		// 2-d array
+		int[][] arr2 = { { 1, 2, 3 }, { 1, 2, 3 }, { 1, 2, 3 } };
+		int add2 = 0;
+
+		for (int i = 0; i < arr2.length; i++) {
+			for (int j = 0; j < arr2.length; j++) {
+				add2 += arr2[i][j];
+			}
+		}
+		System.out.println("Addtion of all 2 d element " + add2);
+	}
 }
