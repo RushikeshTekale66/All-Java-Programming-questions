2a6afeee7edb1394fe0ce8efaec9d42dd611fdb4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-01 22:55:00 +0530

    Addition of two number

--- Main.java
+++ Main.java
@@ -1,27 +1,8 @@
 class Main{
-    public static void main(String [] args){
-        byte b = 1;
-		System.out.println(b);
-		
-		short s = 1;
-		System.out.println(s);
-		
-		int i = 10;
-		System.out.println(i);
-		
-		long l = 1000000000;
-		System.out.println(l);
-		
-		float f = 10.5f;
-		System.out.println(f);
-		
-		double d = 10.5;
-		System.out.println(d);
-		
-		char c = 'c';
-		System.out.println(c);
-		
-		boolean b1 = true; //float
-		System.out.println(b1);
-    }
+	public static void main(String [] args){
+		int a = 10;
+		int b = 10;
+
+		System.out.println(a+b);
+	}
 }
