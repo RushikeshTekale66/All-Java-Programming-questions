48c72da1cc53dbde65a66541a7ad116b56fd384a
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-01 17:02:57 +0530

    Data  type in java

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 class Main{
     public static void main(String [] args){
-        System.out.println("Hellow world");
+        byte b = 1;
+		System.out.println(b);
+		
+		short s = 1;
+		System.out.println(s);
+		
+		int i = 10;
+		System.out.println(i);
+		
+		long l = 1000000000;
+		System.out.println(l);
+		
+		float f = 10.5f;
+		System.out.println(f);
+		
+		double d = 10.5;
+		System.out.println(d);
+		
+		char c = 'c';
+		System.out.println(c);
+		
+		boolean b1 = true; //float
+		System.out.println(b1);
     }
 }
