9cd2578c64b91a39542117589fca39a7bd2ca43f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-12 11:24:18 +0530

    Switch using month example

--- Main.java
+++ Main.java
@@ -1,8 +1,53 @@
-class Main{
-	public static void main(String [] args){
-		int a = 10;
-		int b = 10;
-
-		System.out.println(a+b);
+import java.util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Month : ");
+		int month = sc.nextInt();
+		
+		switch(month){
+		    case 1:
+		        System.out.println("January");
+		        break;
+		        
+		    case 2:
+		        System.out.println("February");
+		        break;
+		       
+		    case 3:
+		        System.out.print("March");
+		        break;
+		    case 4:
+		        System.out.print("April");
+		        break;
+		    case 5:
+		        System.out.print("May");
+		        break;
+		    case 6:
+		        System.out.print("June");
+		        break;
+		    case 7:
+		        System.out.print("July");
+		        break;
+		    case 8:
+		        System.out.print("August");
+		        break;
+		    case 9:
+		        System.out.print("September");
+		        break;
+		    case 10:
+		        System.out.print("Octomber");
+		        break;
+		    case 11:
+		        System.out.print("November");
+		        break;
+		    case 12:
+		        System.out.print("December");
+		        break;
+		    default : 
+		        System.out.println("Please Enter between 1-12 ");
+            
+		}
 	}
 }
