74896973b7afa9d2a21e71f4901afbfa4ecf0323
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-18 23:43:38 +0530

    Count of character in String

--- Main.java
+++ Main.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
-class Main{
-	public static void main(String [] args){
+
+class Main {
+	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
 		String str = "Rushikesh";
 		System.out.println("Enter character to check : ");
 		char ch = sc.next().charAt(0);
 
-		for(int i=0; i<str.length(); i++){
-			if(str.charAt(i)==ch){
-				System.out.println("Found character in String");
+		int count = 0;
+		for (int i = 0; i < str.length(); i++) {
+			if (str.charAt(i) == ch) {
+				count++;
 			}
 		}
+		System.out.println("Count of character : " + ch + " is " + count);
 
 	}
 }
