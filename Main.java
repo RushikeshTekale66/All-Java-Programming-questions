a909588cc53c8547b73753ce366b83a8becd7055
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-18 23:41:04 +0530

    Check character present in string

--- Main.java
+++ Main.java
@@ -1,32 +1,17 @@
+import java.util.Scanner;
+class Main{
+	public static void main(String [] args){
+		Scanner sc = new Scanner(System.in);
 
-class Main
-{
-	public static void main(String[] args) {
-		int [] arr = {1, 10};
-		for(int i=0; i<arr.length; i++){
-		    for(int j=i+1; j<arr.length; j++){
-		        if(arr[i]>arr[j]){
-		            int temp = arr[i];
-		            arr[i] = arr[j];
-		            arr[j] = temp;
-		        }
-		    }
-		    System.out.println(arr[i]);
+		String str = "Rushikesh";
+		System.out.println("Enter character to check : ");
+		char ch = sc.next().charAt(0);
+
+		for(int i=0; i<str.length(); i++){
+			if(str.charAt(i)==ch){
+				System.out.println("Found character in String");
+			}
 		}
-		
-// 		System.out.println(arr[arr.length-1]);
-		int count = 0;
-		for(int i=arr[0]; i<=arr[arr.length-1]; i++){
-		    boolean result = true;
-		    for(int j=0; j<arr.length; j++){
-		        if(i==arr[j]){
-		            result = false;
-		        }
-		    }
-		    if(result){
-		        count ++;
-		    }
-		}
-		System.out.println(" Total count " + count);
+
 	}
 }
