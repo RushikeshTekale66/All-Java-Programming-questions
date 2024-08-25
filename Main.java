6389f6a023c58836f08ef57b79b357ed922192e1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-25 23:46:17 +0530

    Addtion of two number

--- Main.java
+++ Main.java
@@ -2,19 +2,9 @@ import java.util.Scanner;
 
 class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		String str = "Rushikesh";
-		System.out.println("Enter character to check : ");
-		char ch = sc.next().charAt(0);
-
-		int count = 0;
-		for (int i = 0; i < str.length(); i++) {
-			if (str.charAt(i) == ch) {
-				count++;
-			}
-		}
-		System.out.println("Count of character : " + ch + " is " + count);
+		int a = 10;
+		int b = 10;
+		System.out.println(a+b);
 
 	}
 }
