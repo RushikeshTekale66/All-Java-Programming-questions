60bd9217966504d0b79465552a96463c9fa4426d
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-25 23:48:16 +0530

    Addtion of two number with scanner input

--- Main.java
+++ Main.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 class Main {
 	public static void main(String[] args) {
-		int a = 10;
-		int b = 10;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number");
+		int a = sc.nextInt();
+		System.out.println("Enter second number");
+		int b = sc.nextInt();
 		System.out.println(a+b);
 
 	}
