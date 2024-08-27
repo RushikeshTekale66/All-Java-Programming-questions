d03e888cc126ecbf67b09a3f4e531fccd45d2ad7
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-08-27 12:25:54 +0530

    Factorial of number : Recursion

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
+
 import java.util.Scanner;
-
-class Main {
+public class Main
+{
+    
+    public static int factorial(int n){
+        if(n==0 || n==1) return 1;
+        else{
+            return n * factorial(n-1);
+        }
+    }
+    
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter first number");
-		int a = sc.nextInt();
-		System.out.println("Enter second number");
-		int b = sc.nextInt();
-		System.out.println(a+b);
-
+		Scanner sc = new Scanner (System.in);
+		
+		System.out.println("Enter the number to check the factorial");
+		int num = sc.nextInt();
+		System.out.println(factorial(num));
 	}
 }
