3dd1a26ef70eb13c0f0b2c83ea3961424b3c1083
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-04 09:12:18 +0530

    String palindrome

--- Main.java
+++ Main.java
@@ -1,20 +1,18 @@
+/**
+ * Main
+ */
+public class Main {
 
-import java.util.Scanner;
-public class Main
-{
-    
-    public static int factorial(int n){
-        if(n==0 || n==1) return 1;
-        else{
-            return n * factorial(n-1);
+    public static void main(String[] args) {
+        String str = "iihhii";
+        boolean isPalindrome = true;
+
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)!=str.charAt(str.length()-i-1)){
+                isPalindrome = false;
+            }
         }
+
+        System.out.println(isPalindrome);
     }
-    
-	public static void main(String[] args) {
-		Scanner sc = new Scanner (System.in);
-		
-		System.out.println("Enter the number to check the factorial");
-		int num = sc.nextInt();
-		System.out.println(factorial(num));
-	}
 }
