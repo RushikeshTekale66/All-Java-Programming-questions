793da069985e452836e4569d3c4bd0f3144631d6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-08 10:52:52 +0530

    Binary Search DSA

--- Main.java
+++ Main.java
@@ -1,8 +1,31 @@
 class Main{
     public static void main(String[] args) {
-        String str = "abcd";  
-        for(int i=0; i<str.length(); i++){
-            System.out.println((int) str.charAt(i)); 
+        int [] arr = {1, 2, 3, 5, 6, 7, 8, 9 };
+        int target = 7;
+
+        int left = 0;
+        int right = arr.length -1 ;
+
+        boolean result = false;
+
+        while(left<right){
+            int mid = (left+right)/2;
+
+            if(target == arr[mid]) result = true;
+
+            if(target<arr[mid]){
+                right = mid -1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+
+        if(result){
+            System.out.println("Target is present in array");
+        }
+        else{
+            System.out.println("Target is not present in array");
         }
     }
 }
