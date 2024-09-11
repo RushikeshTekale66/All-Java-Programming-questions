e3ac9901f7f9111edac658442331f64101d079d4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-11 21:38:57 +0530

    Print string character

--- Main.java
+++ Main.java
@@ -1,29 +1,8 @@
-import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args) {
-		Scanner sc  = new Scanner(System.in);
-
-        int age =  sc.nextInt();
-        
-        if(age>21){
-            System.out.println("Berojgar");
+class Main{
+    public static void main(String [] args){
+        String str = "Rushikesh";
+        for(int i =0; i<str.length(); i++){
+            System.out.println(str.charAt(i));
         }
-        else if(age>18){
-            System.out.println("Young");
-        }
-        else if (age>10){
-            System.out.println("Tean ager");
-        }
-        else if(age>5){
-            System.out.println("Child");
-        }
-        else if(age>=1){
-            System.out.println("Baby");
-        }
-        else{
-            System.out.println("New born");
-        }
-        
-	}
+    }
 }
