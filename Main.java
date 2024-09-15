ff262dd3537107f68c449eec16c287dcff89337c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-15 10:15:59 +0530

    Selection sort

--- Main.java
+++ Main.java
@@ -1,47 +1,44 @@
 import java.util.Scanner;
 
-class Main{
+class Main {
 
-    public void bubblesort(int[] arr){
-        boolean swaped = false;
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr.length-i-1; j++){
-                if(arr[j]>arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                    swaped = true;
+    public void selectionsort(int[] arr) {
+            for (int i = 0; i < arr.length; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[j]<arr[minIndex]){
+                    minIndex = j;
                 }
             }
-            if(!swaped){
-                System.out.println("Array is already sorted");
-                break;
-            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
     }
 
-    //Display the sorted array
-    public void display(int [] arr){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] +  ", ");
+    // Display the sorted array
+    public void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + ", ");
         }
     }
-    public static void main(String [] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Enter the size of the array");
         int size = sc.nextInt();
 
-        int [] arr = new int[size];
+        int[] arr = new int[size];
 
-        //Enter the elements to the array
-        for(int i=0; i<size; i++){
+        // Enter the elements to the array
+        for (int i = 0; i < size; i++) {
             arr[i] = sc.nextInt();
         }
 
-        //object of bubble sort
+        // object of bubble sort
         Main m = new Main();
-        m.bubblesort(arr);
+        m.selectionsort(arr);
 
         System.out.println("Sorted Array is");
         m.display(arr);
