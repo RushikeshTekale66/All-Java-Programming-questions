59d7e12d61a6b4e7b723665b5b8cc0759438d176
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-15 10:06:05 +0530

    Bubble sort Algorithm

--- Main.java
+++ Main.java
@@ -1,11 +1,49 @@
+import java.util.Scanner;
+
 class Main{
+
+    public void bubblesort(int[] arr){
+        boolean swaped = false;
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length-i-1; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swaped = true;
+                }
+            }
+            if(!swaped){
+                System.out.println("Array is already sorted");
+                break;
+            }
+        }
+    }
+
+    //Display the sorted array
+    public void display(int [] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] +  ", ");
+        }
+    }
     public static void main(String [] args){
-        String str = "RushikeshR";
-        if(str.charAt(0)==str.charAt(str.length()-1)){
-            System.out.println("Start and end are equal");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the size of the array");
+        int size = sc.nextInt();
+
+        int [] arr = new int[size];
+
+        //Enter the elements to the array
+        for(int i=0; i<size; i++){
+            arr[i] = sc.nextInt();
         }
-        else{
-            System.out.println("Start & end are not equal");
-        }
+
+        //object of bubble sort
+        Main m = new Main();
+        m.bubblesort(arr);
+
+        System.out.println("Sorted Array is");
+        m.display(arr);
     }
 }
