e0185bb664991a509e0ed50aed1f756d835ff35a
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-16 22:58:38 +0530

    Array Element

--- Main.java
+++ Main.java
@@ -1,45 +1,9 @@
-import java.util.Scanner;
+class Main{
+    public static void main(String [] args){
+        int [] arr = {1, 2, 3, 5, 6, 6, 7, 9};
 
-class Main {
-
-    public void insertionsort(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            int key = arr[i];
-            int j = i - 1;
-
-            while (j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j = j - 1;
-            }
-            arr[j + 1] = key;
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]);
         }
-    }
-
-    // Display the sorted array
-    public void display(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + ", ");
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Enter the size of the array");
-        int size = sc.nextInt();
-
-        int[] arr = new int[size];
-
-        // Enter the elements to the array
-        for (int i = 0; i < size; i++) {
-            arr[i] = sc.nextInt();
-        }
-
-        // object of sort
-        Main m = new Main();
-        m.insertionsort(arr);
-
-        System.out.println("Sorted Array is");
-        m.display(arr);
     }
 }
