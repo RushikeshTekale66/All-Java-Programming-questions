7996baebe976e1cbaf59c417d7257f21ff1f2c14
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-17 17:35:30 +0530

    Print middle element in array

--- Main.java
+++ Main.java
@@ -5,13 +5,6 @@ class Main{
 
         int [] arr = {1, 2, 3, 5, 6, 6, 7, 9};
 
-        int max = 0;
-
-        for(int i=0; i<arr.length; i++){
-            if(max < arr[i]){
-                max=arr[i];
-            }
-        }
-        System.out.println("Max element is : " + max);
+        System.out.println(arr[(arr.length)/2]);        
     }
 }
