a47a8896182063dda80f7a27e62cd75fe807a7e7
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-17 17:29:04 +0530

    find the element in array

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 class Main{
     public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+
         int [] arr = {1, 2, 3, 5, 6, 6, 7, 9};
 
+        System.out.println("Enter the element to find out in Array : ");
+        int target = sc.nextInt();
+        
+        boolean result = false;
+
         for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]);
+            if(arr[i]==target){
+                result = true;
+            }
         }
+
+        if(result) System.out.println("Element is present in array");
+        else System.out.println("Element is not present in array");
     }
 }
