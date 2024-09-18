4810e4ed1ef3085bdf583b1feee62fbc0ae3e692
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-18 18:51:12 +0530

    Three dimentional array

--- Main.java
+++ Main.java
@@ -3,19 +3,24 @@ import java.util.Scanner;
 class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[][] arr = new int[5][5];
+        int [][][] arr = new int[2][2][2];
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.println("Enter the element for the position : " + "Row = " + i + ", Col = " + j);
-                arr[i][j] = sc.nextInt();
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                for(int k=0; k<arr[i][j].length; k++){
+                        System.out.print("Enter the element for next position");
+                        arr[i][j][k] = sc.nextInt();
+                }
             }
         }
 
-        //Print 2D array
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                System.out.print(arr[i][j] + ", ");
+        //Print array
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                for(int k=0; k<arr[i][j].length; k++){
+                    System.out.print(arr[i][j][k] + ", ");
+                }
+                System.out.println();
             }
             System.out.println();
         }
