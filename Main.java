975bfcf5db4b7a6270904097d3e88531d2bf474b
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-18 18:42:46 +0530

    Two Dimentional Array

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
-class Main{
-    public static void main(String [] args){
-       int [] arr1 = new int [5];
-       for(int i=0; i<arr1.length; i++){
-        System.out.print(arr1[i] + " , ");
-       }  
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[5][5];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.println("Enter the element for the position : " + "Row = " + i + ", Col = " + j);
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        //Print 2D array
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + ", ");
+            }
+            System.out.println();
+        }
     }
 }
