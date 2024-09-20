46d450ad1a3df2dddc99f1b02b59051f0108a58f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-20 19:42:11 +0530

    String ==

--- Main.java
+++ Main.java
@@ -1,28 +1,19 @@
-import java.util.Scanner;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int [][][] arr = new int[2][2][2];
-
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[i].length; j++){
-                for(int k=0; k<arr[i][j].length; k++){
-                        System.out.print("Enter the element for next position");
-                        arr[i][j][k] = sc.nextInt();
-                }
-            }
+class Main{
+    public static void main(String [] args){
+        String str = "Rushikesh";
+        String str2 = "Rushikesh";
+        if(str.equals(str2)){
+            System.out.println("String are equal");
+        }
+        else{
+            System.out.println("String are not equal");
         }
 
-        //Print array
-        for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[i].length; j++){
-                for(int k=0; k<arr[i][j].length; k++){
-                    System.out.print(arr[i][j][k] + ", ");
-                }
-                System.out.println();
-            }
-            System.out.println();
+        if(str==str2){
+            System.out.println("String are equal");
+        }
+        else{
+            System.out.println("String are not equal");
         }
     }
 }
