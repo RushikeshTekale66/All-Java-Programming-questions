89f8c12b92e2d327f3ec2a5abec525d0f82c876a
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-28 10:09:16 +0530

    Max element in array

--- Main.java
+++ Main.java
@@ -1,16 +1,14 @@
-class Main{
-    public static void main(String [] args){
-        int [] arr = {11, 33, 55,44,  22, 66};
+class Main {
+    public static void main(String[] args) {
+        int[] arr = { 11, 33, 55, 44, 22, 66 };
 
-        for(int i=0; i<arr.length; i++){
-            for(int j=i+1; j<arr.length; j++){
-                if(arr[i]<arr[j]){
-                    int temp = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = temp;
-                }
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
             }
-            System.out.println(arr[i] + ", ");
         }
+        System.out.println("Max element is " + max);
     }
 }
