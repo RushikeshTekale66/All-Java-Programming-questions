3fba9c566adb563f487047947740c16f02cdc5b3
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-29 17:31:45 +0530

    Thread by extending Thread class

--- Main.java
+++ Main.java
@@ -1,20 +1,35 @@
+class MyThread1 extends Thread{
+    public void run(){
+        for(int i=0; i<5; i++){
+            System.out.println("Thread 1 : index : " + i);
+        }
+    }
+}
 
-class Main{
-    public Main(){
-        System.out.println("Constructor");
+class MyThread2 extends Thread{
+    public void run(){
+        for(int i=0; i<5; i++){
+            System.out.println("Thread 2 : index : "+ i);
+        }
     }
-    static{
-        System.out.println("Static block");
-    }
-    public void msg2(){
-        System.out.println("Instance block");
-    }
-    {
-        System.out.println("Block");
-    }
-    public static void main(String[] args) {
-       Main m = new Main();
+}
 
-       m.msg2();
+/**
+ * Main
+ */
+public class Main {
+
+    public static void main(String [] args){
+        MyThread1 m = new MyThread1();
+        m.start();
+        System.out.println( "Class Name of thread 1 is : " + m.getName());
+        System.out.println("Priority of thread 1 is : " + m.getPriority());
+        System.out.println("State og thread 1 is : " + m.getState());
+
+        MyThread2 m2 = new MyThread2();
+        m2.start();
+        System.out.println( "Class Name of thread 2 is : " + m2.getName());
+        System.out.println("Priority of thread 2 is : " + m2.getPriority());
+        System.out.println("State of thread 2 is : " + m2.getState());
     }
 }
