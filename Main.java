de1c00c4c8a54ac299440247160c6f59326afc85
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-09-29 11:58:10 +0530

    final

--- Main.java
+++ Main.java
@@ -1,24 +1,7 @@
 
-class Addition extends RuntimeException{
-    public Addition(String msg){
-        super(msg);
-    }
-}
-
-class Main {
-    public void sum(){
-        int a = 10;
-        int b = 0;
-        try {
-            System.out.println(a + b);
-            throw new Addition("Error in addition");
-        } catch (Exception e) {
-            System.out.println("Error " + e);
-        }
-    }
-
+class Main{
     public static void main(String[] args) {
-            Main m = new Main();
-            m.sum();
+        final int a = 10;
+        System.out.println(a);
     }
 }
