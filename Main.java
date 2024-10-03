8eda62bf597c790e9a59d1f5482403c4ffadc3c4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-03 23:56:42 +0530

    String length

--- Main.java
+++ Main.java
@@ -1,8 +1,6 @@
 class Main{
     public static void main(String [] args){
         String str = "Rushikesh";
-        for(int i=0; i<str.length(); i++){
-            System.out.print(str.charAt(i));
-        }
+            System.out.print(str.length());
     }
 }
