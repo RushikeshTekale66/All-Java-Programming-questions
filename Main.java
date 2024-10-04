d76c37f270178b2b05487ba8347bbe76f655e95e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-04 15:48:08 +0530

    Print String

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String [] args){
         String str = "Rushikesh";
             System.out.print(str.length());
+            System.out.print(str);
     }
 }
