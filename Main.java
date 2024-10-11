ac513bd65c583e69bf793640334e9c185f35445a
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-11 14:30:50 +0530

    Print string

--- Main.java
+++ Main.java
@@ -1,14 +1,10 @@
-class Main{
+/**
+ * Main
+ */
+public class Main {
+
     public static void main(String [] args){
         String str = "Rushikesh";
-<<<<<<< HEAD
-        String str2 = "";
-        for(int i=0; i<str.length(); i++){
-            str2 =str.charAt(i) + str2 ;
-        }
-        System.out.println(str2);
-=======
-            System.out.print(str.length());
->>>>>>> c7f92f84ee8c5c5b1956c6303d506b9317de64ea
+        System.out.println(str);
     }
 }
