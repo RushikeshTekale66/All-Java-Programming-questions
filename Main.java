9ffdbf23ee99bac8166a676dca73d147fd04665e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-13 11:05:27 +0530

    Reverse String

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
 
     public static void main(String [] args){
         String str = "Rushikesh";
-        System.out.println(str);
+        for(int i=str.length()-1; i>=0; i--){
+            System.out.println(str.charAt(i));
+        }
     }
 }
