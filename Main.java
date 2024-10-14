819f8886033b9faebb9589a3e5c2d91154c2a20f
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-14 10:26:25 +0530

    Duplicate characters in the string

--- Main.java
+++ Main.java
@@ -5,8 +5,12 @@ public class Main {
 
     public static void main(String [] args){
         String str = "Rushikesh";
-        for(int i=str.length()-1; i>=0; i--){
-            System.out.println(str.charAt(i));
+        for(int i=0; i<str.length(); i++){
+            for(int j=i+1; j<str.length(); j++){
+                if(str.charAt(i)==str.charAt(j)){
+                    System.out.println(str.charAt(i));
+                }
+            }
         }
     }
 }
