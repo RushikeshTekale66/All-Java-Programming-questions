6a643e2b89eac4ae18e6626707a8690aa787823c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-16 10:39:47 +0530

    Print string

--- Main.java
+++ Main.java
@@ -4,14 +4,9 @@
 public class Main {
 
     public static void main(String [] args){
-        String str = "rusur";
-        boolean result = true;
-        for(int i=0; i<str.length()/2; i++){
-            if(str.charAt(i)!=str.charAt(str.length()-1-i)){
-                result = false;
-            }
+        String str = "Rushikesh";
+        for(int i=0; i<str.length(); i++){
+            System.out.print(str.charAt(i));
         }
-        if(result) System.out.println("Palindrome");
-        else System.out.println("Not palindrome");
     }
 }
