994ccae540b9eb2c48a582c82214a1af76fe4287
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-20 21:41:25 +0530

    Overloading

--- Main.java
+++ Main.java
@@ -2,39 +2,23 @@
  * Main
  */
 
- class Parent{
-    int id = 10;
-    String name = "Rushikesh";
+class OverLoad {
 
-    public void display(){
-        System.out.println(id);
-        System.out.println(name);
+    public void MathOP(int a, int b) {
+        System.out.println("Addition is : " + (a + b));
     }
- }
 
- class Child extends Parent{
-    String city = "Pune";
-
-    public void display2(){
-        System.out.println(city);
+    //Overloading
+    public void MathOP(int a, double b) {
+        System.out.println("Substraction is : " + (a-b));
     }
- }
+}
 
- class Child2 extends Parent{
-    String city = "Dharashiv";
-
-    public void display3(){
-        System.out.println(city);
-    }
- }
 public class Main {
-    public static void main(String [] args){
-        Child c = new Child();
-        c.display();
-        c.display2();
 
-        Child2 c2 = new Child2();
-        c2.display();
-        c2.display3();
+    public static void main(String[] args) {
+        OverLoad o = new OverLoad();
+        o.MathOP(10, 5.5);
+        o.MathOP(10, 10);
     }
 }
