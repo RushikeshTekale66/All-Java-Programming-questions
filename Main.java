14d8f4bd971379bdf0c936934eb0018e5d7a52d5
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-20 21:56:52 +0530

    Method overriding

--- Main.java
+++ Main.java
@@ -2,23 +2,45 @@
  * Main
  */
 
-class OverLoad {
+ class Person{
+    String name;
+    int age;
 
-    public void MathOP(int a, int b) {
-        System.out.println("Addition is : " + (a + b));
+    public Person(String name, int age){
+        this.name = name;
+        this.age = age;
     }
 
-    //Overloading
-    public void MathOP(int a, double b) {
-        System.out.println("Substraction is : " + (a-b));
+    public void show(){
+        System.out.println(name);
+        System.out.println(age);
     }
-}
+ }
+
+ class Child extends Person{
+    double salary;
+    double exp;
+
+    public Child(double salary, double exp, String name, int age){
+        super(name, age);
+        this.salary = salary;
+        this.exp = exp;
+    }
+
+    @Override
+    public void show(){
+        System.out.println(this.name);
+        System.out.println(this.age);
+        System.out.println(this.salary);
+        System.out.println(this.exp);
+    }
+
+ }
 
 public class Main {
 
-    public static void main(String[] args) {
-        OverLoad o = new OverLoad();
-        o.MathOP(10, 5.5);
-        o.MathOP(10, 10);
+    public static void main(String [] args){
+        Child c = new Child(10000, 4, "Rushikesh", 10);
+        c.show();
     }
 }
