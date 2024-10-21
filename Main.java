e3dd43a93b5dd00ec1d82f5ad014cf4a8bebf510
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-21 20:50:17 +0530

    Rotate array right to left

--- Main.java
+++ Main.java
@@ -1,17 +1,28 @@
-
+/**
+ * Main
+ */
 public class Main {
+    public void rotate(int [] arr, int k){
+        reverse(arr, 0, arr.length-1);
+        reverse(arr, 0, k-1);
+        reverse(arr, k, arr.length-1);
+    }
 
-    public static void main(String [] args){
-        int [] arr = {1, 2, 3, 4};
-        int k =1;
-
-        for(int i=0; i<k; i++){
-            int first = arr[0], j;
-                for(j=0; j<arr.length-1; j++){
-                    arr[j] = arr[j+1];
-                }
-                arr[j] = first;
+    public void reverse(int [] arr, int start, int end){
+        while(start<end){
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
+    }
+    public static void main(String [] args){
+        int [] arr = {1, 2, 3, 4, 5};
+        int k = 2;
+
+        Main m = new Main();
+        m.rotate(arr, k);
 
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i] + ", ");
