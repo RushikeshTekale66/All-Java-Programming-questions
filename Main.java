add130fb77d116810a4e1dc4f00ce6d75f3b0cd4
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-21 20:44:32 +0530

    Swap the array left to right

--- Main.java
+++ Main.java
@@ -1,25 +1,20 @@
-/**
- * Main
- */
 
- interface Animal{
-    public abstract void sound();
-    public abstract void sum(int a, int b);
- }
-
- class Lion implements Animal{
-    public void sound(){
-        System.err.println("Ok OOk");
-    }
-    public void sum(int a, int b){
-        System.out.println(a+b);
-    }
- }
 public class Main {
 
     public static void main(String [] args){
-        Animal l = new Lion();
-        l.sound();
-        l.sum(10, 20);
+        int [] arr = {1, 2, 3, 4};
+        int k =1;
+
+        for(int i=0; i<k; i++){
+            int first = arr[0], j;
+                for(j=0; j<arr.length-1; j++){
+                    arr[j] = arr[j+1];
+                }
+                arr[j] = first;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + ", ");
+        }
     }
 }
