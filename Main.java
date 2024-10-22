cc38c55cce2b996eaeacbc021f0f2b01204a8ea6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-22 23:01:16 +0530

    Right shift the array

--- Main.java
+++ Main.java
@@ -1,20 +1,33 @@
-class Main{
-    public static void main(String [] args){
-        int[] arr= {1, 2, 3, 4, 5, 6, 7, 8, 9};
+class Main {
+    public void operation(int[] arr, int k) {
+        reverse(arr, 0, arr.length-1);
+        reverse(arr, 0, k-1);
+        reverse(arr, k, arr.length-1);
+    }
 
-        int k=2;
-
-        for(int i=0; i<k; i++){
-            int first = arr[0], j;
-            for(j=0; j<arr.length-1; j++){
-                arr[j] = arr[j+1];
-            }
-            arr[arr.length-1] = first;
+    public void reverse(int [] arr, int start, int end) {
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
         }
+    }
 
-        // Display array
+    public void display(int [] arr){
         for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i]);
+            System.out.print(arr[i] + ", ");
         }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        int k = 2;
+
+        Main m = new Main();
+        m.operation(arr, k);
+        m.display(arr);
+
     }
 }
