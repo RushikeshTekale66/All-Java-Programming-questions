7287c28cff035b9857c7cf5ded122bdcdff48c57
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-22 22:32:20 +0530

    left shift the array

--- Main.java
+++ Main.java
@@ -1,31 +1,20 @@
-/**
- * Main
- */
-public class Main {
-    public void rotate(int [] arr, int k){
-        reverse(arr, 0, arr.length-1);
-        reverse(arr, 0, k-1);
-        reverse(arr, k, arr.length-1);
-    }
-
-    public void reverse(int [] arr, int start, int end){
-        while(start<end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-    }
+class Main{
     public static void main(String [] args){
-        int [] arr = {1, 2, 3, 4, 5};
-        int k = 2;
+        int[] arr= {1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-        Main m = new Main();
-        m.rotate(arr, k);
+        int k=2;
 
+        for(int i=0; i<k; i++){
+            int first = arr[0], j;
+            for(j=0; j<arr.length-1; j++){
+                arr[j] = arr[j+1];
+            }
+            arr[arr.length-1] = first;
+        }
+
+        // Display array
         for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + ", ");
+            System.out.print(arr[i]);
         }
     }
 }
