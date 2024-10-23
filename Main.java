0d1df8c4a29aa9ed2a7ce27fe1c478d99f8aa4ba
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-23 09:44:04 +0530

    Last element of an array

--- Main.java
+++ Main.java
@@ -1,33 +1,6 @@
-class Main {
-    public void operation(int[] arr, int k) {
-        reverse(arr, 0, arr.length-1);
-        reverse(arr, 0, k-1);
-        reverse(arr, k, arr.length-1);
-    }
-
-    public void reverse(int [] arr, int start, int end) {
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
-    public void display(int [] arr){
-        for(int i=0; i<arr.length; i++){
-            System.out.print(arr[i] + ", ");
-        }
-    }
-
-    public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
-        int k = 2;
-
-        Main m = new Main();
-        m.operation(arr, k);
-        m.display(arr);
-
+class Main{
+    public static void main(String [] args){
+        int [] arr = {1, 2, 3, 4, 5};
+        System.out.println("Last element of an array : " + arr[arr.length-1]);
     }
 }
