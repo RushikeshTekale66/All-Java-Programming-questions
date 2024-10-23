adc2a6c122cea2ee0eb09b34f4bd9f060ade4e16
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-23 20:08:36 +0530

    Print string use for loop

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main{
     public static void main(String [] args){
        String str = "Rushikesh";
-       System.out.println(str);
+       for(int i=0; i<str.length(); i++){
+        System.out.print(str.charAt(i));
+       }
     }
 }
