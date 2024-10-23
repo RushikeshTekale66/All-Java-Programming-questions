51c7c8d37530790744eae2e9445b9b381124f14a
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-23 20:06:58 +0530

    Print String

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main{
     public static void main(String [] args){
-        int [] arr = {1, 2, 3, 4, 5};
-        System.out.println("Last element of an array : " + arr[arr.length-1]);
+       String str = "Rushikesh";
+       System.out.println(str);
     }
 }
