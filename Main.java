d31d0d69c96352d324ea54c2ee90289471d1cb0d
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-10-30 19:10:09 +0530

    Sort array in ascending order

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
-class Main{
-    public static void main(String [] args){
-       int [] arr = {1, 2, 3, 4, 5, 6, 7, 7};
-       for(int i=0; i<arr.length; i++){
-        for(int j=i+1; j<arr.length; j++){
-            if(arr[i]==arr[j]){
-                System.out.println(arr[i]);
+class Main {
+    public static void main(String[] args) {
+        int[] arr = { 10, 2, 3, 4, 5, 6, 7, 7 };
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
             }
         }
-       }
 
+
+        //Print array
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
     }
 }
