e2707108a8d6cabe6e58cac2d42729128613ccb7
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-10 23:41:17 +0530

    Last element of array

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 class Main {
     public static void main(String[] args) {
         int[] arr = { 10, 2, 3, 4, 5, 6, 7, 7 };
-        System.out.println(arr[arr.length-2]);
+        System.out.println(arr[arr.length-1]);
     }
 }
