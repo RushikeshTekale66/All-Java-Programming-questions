fb47d7173de412018a03f61141f9ea1a234d7e34
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-11 10:38:05 +0530

    Duplicate elment in array

--- Main.java
+++ Main.java
@@ -1,6 +1,12 @@
 class Main {
     public static void main(String[] args) {
-        int[] arr = { 10, 2, 3, 4, 5, 6, 7, 7 };
-        System.out.println(arr.length);
+        int[] arr = { 10, 10, 2, 3, 4, 5, 6, 7, 7 };
+        for(int i=0; i<arr.length; i++){
+            for(int j=i+1; j<arr.length; j++){
+                if(arr[i]==arr[j]){
+                    System.out.println(arr[i]);
+                }
+            }
+        }
     }
 }
