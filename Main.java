bb1aea51b36c5fd840e6ad4fca33e762ca498921
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-11 10:41:40 +0530

    Max occurence of element

--- Main.java
+++ Main.java
@@ -1,12 +1,21 @@
 class Main {
     public static void main(String[] args) {
-        int[] arr = { 10, 10, 2, 3, 4, 5, 6, 7, 7 };
+        int[] arr = { 10, 10, 2, 3, 4, 5, 6, 7, 7 ,7};
+
+        int max =0;
+        int element = arr[0];
         for(int i=0; i<arr.length; i++){
+            int count =0;
             for(int j=i+1; j<arr.length; j++){
                 if(arr[i]==arr[j]){
-                    System.out.println(arr[i]);
+                    count++;
                 }
             }
+            if(max<count){
+                max+=count;
+                element = arr[i];
+            }
         }
+        System.out.println("Max occurence of element is : " + element + " & count is : " + max);
     }
 }
