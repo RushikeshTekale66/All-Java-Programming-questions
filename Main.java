467ac1390483aa8e0e5f2cca056ba62b491de2ee
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-21 23:50:56 +0530

    Common element from two string

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 public class Main{
 	public static void main(String[] args) {
-		String str = "Rushikesh";
-		for(int i=0; i<str.length(); i++){
-		    for(int j=i+1; j<str.length(); j++){
-		        if(str.charAt(i)==str.charAt(j)){
-		            System.out.println(str.charAt(i));
+		int [] arr1 = {1, 2, 3, 4};
+		int [] arr2 = {1, 3, 5, 6};
+		for(int i=0; i<arr1.length; i++){
+		    for(int j=0; j<arr2.length; j++){
+		        if(arr1[i]==arr2[j]){
+		            System.out.println(arr1[i]);
 		        }
 		    }
 		}
 	}
 }
+//O/P :1, 2
