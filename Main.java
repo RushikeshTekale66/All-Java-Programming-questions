490fa142b18b741e78254cda5917096e56032529
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-24 21:45:22 +0530

    Print string in reverse

--- Main.java
+++ Main.java
@@ -1,19 +1,9 @@
-// Sort String
 public class Main{
 	public static void main(String[] args) {
 		String str1 = "Tekale";
-		char[] str = str1.toCharArray();
-		for(int i=0; i<str.length; i++){
-		    for(int j=i+1; j<str.length; j++){
-		        if(str[i]>str[j]){
-		            char temp = str[i];
-		            str[i] = str[j];
-		            str[j]=temp;
-		        }
-		    }
+		for(int i=str1.length()-1; i>=0; i--){
+			System.out.print(str1.charAt(i));
 		}
-		System.out.println(new String (str));
 		
 	}
 }
-//O/P :aeeklt
