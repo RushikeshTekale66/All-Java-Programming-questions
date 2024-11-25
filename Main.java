95f79f9808d944fff359ea055f824deeb017e726
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-25 19:46:18 +0530

    Print fobonachi series up to n number

--- Main.java
+++ Main.java
@@ -1,10 +1,18 @@
 public class Main{
 
-	public static int fibonachiNumber(int n){
-		if(n<=1) return n;
-		return fibonachiNumber(n-1)+fibonachiNumber(n-2);
+	public static void fibonachiNumber(int n){
+		int n1 =1, n2 = 1;
+
+		System.out.print(n1+ ", "+n2);
+
+		for(int i=2; i<n; i++){
+			int sum = n1+n2;
+			System.out.print(", "+sum);
+			n1=n2;
+			n2=sum;
+		}
 	}
 	public static void main(String[] args) {
-		System.out.println(Main.fibonachiNumber(3));
+		Main.fibonachiNumber(10);
 	}
 }
