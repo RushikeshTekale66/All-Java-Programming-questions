fa2772e9768ad419e84bf18c7df4a99348c0b2cb
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-11-26 10:09:53 +0530

    hello

--- Main.java
+++ Main.java
@@ -1,18 +1,5 @@
 public class Main{
-
-	public static void fibonachiNumber(int n){
-		int n1 =1, n2 = 1;
-
-		System.out.print(n1+ ", "+n2);
-
-		for(int i=2; i<n; i++){
-			int sum = n1+n2;
-			System.out.print(", "+sum);
-			n1=n2;
-			n2=sum;
-		}
-	}
 	public static void main(String[] args) {
-		Main.fibonachiNumber(10);
+		System.out.println("Hello");
 	}
 }
