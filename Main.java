621a854d1a680302ef4ccf2968cb22d2f48161d2
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-12-03 17:52:57 +0530

    Even Odd

--- Main.java
+++ Main.java
@@ -2,24 +2,22 @@ import java.util.Scanner;
 
 public class Main{
 
-	public static void swap(int a, int b){
-		a = a+b;
-		b=a-b;
-		a=a-b;
-
-		System.out.println("Values of after swaping is : a = "+ a + " & b = " +b );
+	public static void Even(int a){
+		if(a%2==0){
+			System.out.println("The number is even");
+		}
+		else{
+			System.out.println("The number is Odd");
+		}
 	}
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter first number a");
+		System.out.println("Enter first number");
 		int a = sc.nextInt();
 
-		System.out.println("Enter second number b");
-		int b = sc.nextInt();
-
-		Main.swap(a, b);
+		Main.Even(a);
 
 
 	}
