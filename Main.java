22cc9847122b394e17eeb77fc47fe280194385c9
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-12-05 18:55:03 +0530

    Insert element into array

--- Main.java
+++ Main.java
@@ -1,28 +1,20 @@
 import java.util.Scanner;
 
-public class Main{
-
-	public static void Palindrome(String str){
-		int end = str.length()-1;
-		int start = 0;
-		boolean result = true;
-		while(start<end){
-			if(str.charAt(start)!=str.charAt(end)){
-				result = false;
-			}
-			start++;
-			end--;
-		}
-		if(result) System.out.println("String is Palindrome");
-		else System.out.println("String is not palindrome");
-	}
-
-	public static void main(String[] args) {
+class Main{
+	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter the String");
-		String str = sc.next();
+		System.out.println("Enter size of array");
+		int size = sc.nextInt();
 
-		Main.Palindrome(str);
+		int[] arr = new int[size];
+		
+		for(int i=0; i<size; i++){
+			arr[i] = sc.nextInt();
+		}
+
+		for(int i=0; i<size; i++){
+			System.out.println(arr[i]+ ", ");
+		}
 	}
 }
