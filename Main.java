717dd359668716ccd3deca95227278dd4e0795f1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-12-06 11:48:47 +0530

    Get the user input & reverse the string

--- Main.java
+++ Main.java
@@ -1,30 +1,13 @@
 import java.util.Scanner;
-
 class Main{
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter size of array");
-		int size = sc.nextInt();
+		System.out.println("Enter the String");
+		String str = sc.nextLine();
 
-		int[] arr = new int[size];
-		
-		for(int i=0; i<size; i++){
-			arr[i] = sc.nextInt();
-		}
-
-		for(int i=0; i<size; i++){
-			for(int j=i+1; j<size; j++){
-				if(arr[i]>arr[j]){
-					int temp = arr[i];
-					arr[i]=arr[j];
-					arr[j]=temp;
-				}
-			}
-		}
-
-		for(int i=0; i<size; i++){
-			System.out.print(arr[i] + ", ");
+		for(int i=str.length()-1; i>=0; i--){
+			System.out.println(str.charAt(i));
 		}
 	}
 }
