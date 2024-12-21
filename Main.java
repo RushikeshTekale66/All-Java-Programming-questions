0b75ba11a64689e1fbf133a27f282eeca76fee1e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2024-12-21 16:52:07 +0530

    Finding duplicate element in arry

--- Main.java
+++ Main.java
@@ -3,13 +3,12 @@ class Main{
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("Enter the String");
-		String str = sc.nextLine();
+		int [] arr={1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1};
 
-		for(int i=0; i<str.length(); i++){
-			for(int j=i+1; j<str.length(); j++){
-				if(str.charAt(i)==str.charAt(j)){
-					System.out.println(str.charAt(i));
+		for(int i=0; i<arr.length; i++ ){
+			for(int j=i+1; j<arr.length; j++){
+				if(arr[i]==arr[j]){
+					System.out.println(arr[i]);
 				}
 			}
 		}
