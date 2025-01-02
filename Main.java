8550b7fb8930e2e4497107f94d21cd252b48efb6
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-02 22:44:23 +0530

    Print array element

--- Main.java
+++ Main.java
@@ -5,8 +5,8 @@ class Main{
 
 		int [] arr={1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1};
 
-		int len = arr.length; 
-
-		System.out.println(arr[len-1]);
+		for(int i=0; i<arr.length; i++){
+			System.out.println(arr[i]);
+		}
 	}
 }
