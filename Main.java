362b1b645530bb8c8f75ecf23c017ed081a1d52e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-05 18:16:47 +0530

    String input in java

--- Main.java
+++ Main.java
@@ -3,10 +3,12 @@ class Main{
 	public static void main(String [] args){
 		Scanner sc = new Scanner(System.in);
 
-		int [] arr={1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1};
+		System.out.println("Line 1");
+		String str1 = sc.nextLine();
+		System.out.println("Line 2");
+		String str2 = sc.next();
 
-		for(int i=arr.length-1; i>=0; i--){
-			System.out.println(arr[i]);
-		}
+		System.out.println(str1);
+		System.out.println(str2);
 	}
 }
