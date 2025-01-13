19519b010a62a6df1a97543d5613043f31dc0bcd
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-13 16:31:32 +0530

    LSD & MSD

--- Main.java
+++ Main.java
@@ -1,41 +1,18 @@
-class Main {
+public class Main {
+
 	public static void main(String[] args) {
-		System.out.println("Three-digit combinations WITH repetition:");
-		generateWithRepetition();
+		int number = 15;
 
-		System.out.println("\nThree-digit combinations WITHOUT repetition:");
-		generateWithoutRepetition();
-	}
+		//Least significant digit
+		int lsd = number%10;
 
-	// Generate combinations with repetition
-	public static void generateWithRepetition() {
-		int count = 0;
-		for (int i = 1; i <= 9; i++) { // First digit (1-9)
-			for (int j = 0; j <= 9; j++) { // Second digit (0-9)
-				for (int k = 0; k <= 9; k++) { // Third digit (0-9)
-					// System.out.println(i + "" + j + "" + k);
-					count++;
-				}
-			}
+		//Most significant digit
+		int msd = number;
+		while(msd>=10){
+			msd/=10;
 		}
-		System.out.println(count);
-	}
 
-	// Generate combinations without repetition
-	public static void generateWithoutRepetition() {
-		int count = 0;
-		for (int i = 1; i <= 9; i++) { // First digit (1-9)
-			for (int j = 0; j <= 9; j++) { // Second digit (0-9)
-				if (j == i)
-					continue; // Ensure no repetition of digits
-				for (int k = 0; k <= 9; k++) { // Third digit (0-9)
-					if (k == i || k == j)
-						continue; // Ensure no repetition of digits
-					// System.out.println(i + "" + j + "" + k);
-					count++;
-				}
-			}
-		}
-		System.out.println(count);
+		System.err.println("LSD : " + lsd);
+		System.out.println("MSD : " + msd);
 	}
 }
