5c87862311ab113092d2c6a00ad1424fe8682b3c
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-13 16:04:07 +0530

    Keystal 1

--- Main.java
+++ Main.java
@@ -1,24 +1,12 @@
-import java.util.Scanner;
 class Main{
+
+	public static void loop(int start,int end){
+		if(start <= end){
+		System.out.println(start);
+		loop(start + 1, end);
+		}
+	}
 	public static void main(String [] args){
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("Enter string to check palindrome");
-		String str = sc.nextLine();
-
-		boolean result = true;
-		
-		for(int i=0; i<str.length()/2; i++){
-			if(str.charAt(i)!=str.charAt(str.length()-1-i)){
-				result = false;
-			}
-		}
-
-		if(result){
-			System.out.println("String is palindrom");
-		}
-		else{
-			System.out.println("String is not palindrom");
-		}
+		loop(11, 20);
 	}
 }
