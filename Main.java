7fbdac13043322a4aff4cd06d713af6eddd00d81
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-13 16:47:02 +0530

    Print the string from array whose first & last letter is same

--- Main.java
+++ Main.java
@@ -1,15 +1,14 @@
 public class Main {
 
 	public static void main(String[] args) {
-		String str = "Rushikesh Tekale";
-		int count = 0;
+		String [] arr = {"geek", "leet", "geekg", "rushir"};
 
-		for (int i = 0; i < str.length(); i++) {
-			char ch = str.charAt(i);
-			if (ch >='a' && ch <= 'z') {
-				count++;
+		for(int i=0; i<arr.length; i++){
+			String str = arr[i];
+			int len = str.length()-1;
+			if(str.charAt(0)==str.charAt(len)){
+				System.out.println(str);
 			}
 		}
-		System.out.println(count);
 	}
 }
