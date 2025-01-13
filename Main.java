0e507d909234c6b9db16a410267ed062e56e68d0
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-13 17:16:35 +0530

    Armstrong number

--- Main.java
+++ Main.java
@@ -1,14 +1,31 @@
-public class Main {
+class Main{
+	public static void main(String [] args){
+		int number = 103; // 153, 9474, 123
 
-	public static void main(String[] args) {
-		String [] arr = {"geek", "leet", "geekg", "rushir"};
-
-		for(int i=0; i<arr.length; i++){
-			String str = arr[i];
-			int len = str.length()-1;
-			if(str.charAt(0)==str.charAt(len)){
-				System.out.println(str);
-			}
+		if(isArmstrong(number)){
+			System.out.println(number + " is an Armstrong number");
 		}
+		else{
+			System.out.println(number + " is not Armstrong number");
+		}
+	}
+
+	//Method to check if a number is an Armstrong number
+	public static boolean isArmstrong(int number){
+		int original = number, sum=0, digits =0;
+
+		int temp = number;
+		while (temp>0) {
+			digits++;
+			temp/=10;
+		}
+
+		temp = number;
+		while (temp>0) {
+			int digit = temp%10;
+			sum+=Math.pow(digit, digits);
+			temp/=10;
+		}
+		return sum==original;
 	}
 }
