013a0b14fd27a848c95d56b5713866091114b5f2
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-16 16:19:49 +0530

    Reverse the words in the string

--- Main.java
+++ Main.java
@@ -3,15 +3,20 @@ public class Main {
     public static void main(String[] args) {
         String str = "Rushikesh Ramesh Tekale";
         String [] arr = str.split(" ");
+
+        String result = "";
         
-        for(int j=arr.length-1; j>=0; j--){
-            String s = arr[j];
-            System.out.print(s + " : ");
+        for(String s : arr){
+            String s1 = "";
             for(int i=s.length()-1; i>=0; i--){
-                System.out.print(s.charAt(i));
+                s1 = s1 + s.charAt(i);
             }
-            System.out.println();
+            result = result + s1 + " ";
         }
+
+        result = result.trim();
+
+        System.out.println(result);
 
        
     }
