044baa76ec63004e07aa2e05d95597542bc70cc1
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-16 15:52:44 +0530

    Rever the word & reverse the character in the string

--- Main.java
+++ Main.java
@@ -4,7 +4,8 @@ public class Main {
         String str = "Rushikesh Ramesh Tekale";
         String [] arr = str.split(" ");
         
-        for(String s : arr){
+        for(int j=arr.length-1; j>=0; j--){
+            String s = arr[j];
             System.out.print(s + " : ");
             for(int i=s.length()-1; i>=0; i--){
                 System.out.print(s.charAt(i));
