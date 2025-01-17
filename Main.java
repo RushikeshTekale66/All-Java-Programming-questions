075691da4492efd9a7a8c5f581aa1cdf6b8c6fce
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-17 10:45:47 +0530

    Print array element

--- Main.java
+++ Main.java
@@ -4,14 +4,8 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         int digits[ ] = {1, 2, 3};
-        for (int i = 0; i < digits.length; i++) {
-            for (int j = 0; j < digits.length; j++) {
-                for (int k = 0; k < digits.length; k++) {
-                    if (i != j && j != k && i != k) { // Ensure unique digits
-                        System.out.println("" + digits[i] + digits[j] + digits[k]);
-                    }
-                }
-            }
+        for(int i : digits){
+            System.out.println(i);
         }
     }
 }
