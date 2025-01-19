2e3a6f9e07b9116ec584ce5bd95eb6944914d65e
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-19 10:53:45 +0530

    Print the string whose first & last character is same

--- Main.java
+++ Main.java
@@ -1,11 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
-        int digits[ ] = {1, 2, 3};
-        for(int i : digits){
-            System.out.println(i);
+        String [] arr = {"Rushi", "tekt"};
+
+        for(int i=0; i<arr.length; i++){
+            String s = arr[i];
+            int len = s.length()-1;
+            if(s.charAt(0)==s.charAt(len)){
+                System.out.println(s);
+            }
         }
     }
 }
