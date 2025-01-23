c92329a1658fe6977a440ca60cafedd7a192bd23
Rushikesh Tekale <98608970+RushikeshTekale66@users.noreply.github.com>
2025-01-23 20:20:21 +0530

    String print

--- Main.java
+++ Main.java
@@ -1,13 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        String [] arr = {"Rushi", "tekt"};
-
-        for(int i=0; i<arr.length; i++){
-            String s = arr[i];
-            int len = s.length()-1;
-            if(s.charAt(0)==s.charAt(len)){
-                System.out.println(s);
-            }
-        }
+        System.out.println("String");
     }
 }
